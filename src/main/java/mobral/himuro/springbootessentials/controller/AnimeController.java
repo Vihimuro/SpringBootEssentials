b5c32c5d9560a0ddac525fc385bcf2ae6d0e5a19
Himuro <vihi1208@gmail.com>
2023-10-04 08:49:12 -0300

    first commit

---
+++ src/main/java/mobral/himuro/springbootessentials/controller/AnimeController.java
@@ -0,0 +1,28 @@
+package mobral.himuro.springbootessentials.controller;
+
+import mobral.himuro.springbootessentials.domain.Anime;
+import mobral.himuro.springbootessentials.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    //localhost:8080/anime/list
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("HxH"), new Anime("One Piece"));
+    }
+}
