3ef17dce62c9ff08a140ace46a343ae1568b21b5
vihimuro <vihi1208@gmail.com>
2023-10-05 10:43:36 -0400

    lesson 16 - Request Params

--- src/main/java/mobral/himuro/springbootessentials/repository/AnimeRepository.java
+++ src/main/java/mobral/himuro/springbootessentials/repository/AnimeRepository.java
@@ -3,6 +3,8 @@ package mobral.himuro.springbootessentials.repository;
 import mobral.himuro.springbootessentials.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimeRepository extends JpaRepository<Anime, Long> {
+import java.util.List;
 
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+    List<Anime> findByName(String name);
 }
